9d7e6d004e5b6e670f6bee9a519e1e5a468e359e
Dobakito <mdobak44@icloud.com>
2021-06-01 16:03:13 -0500

    commit tree 537c9bc7bce19802d646e054de9e40ea03d8600a

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,9 +5,9 @@ require 'rack/test'
 require 'capybara/rspec'
 require 'capybara/dsl'
 
-if defined?(ActiveRecord::Migrator) && ActiveRecord::Migrator.needs_migration?
-  raise 'Migrations are pending run `rake db:migrate SINATRA_ENV=test` to resolve the issue.'
-end
+# if defined?(ActiveRecord::Migrator) && ActiveRecord::Migration.needs_migration?
+#   raise 'Migrations are pending run `rake db:migrate SINATRA_ENV=test` to resolve the issue.'
+# end
 
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
@@ -15,16 +15,16 @@ RSpec.configure do |config|
   config.include Rack::Test::Methods
   config.include Capybara::DSL
 
-  # config.before(:suite) do
-  #   DatabaseCleaner.strategy = :transaction
-  #   DatabaseCleaner.clean_with(:truncation)
-  # end
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
 
-  # config.around(:each) do |example|
-  #   DatabaseCleaner.cleaning do
-  #     example.run
-  #   end
-  # end
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 
   config.order = 'default'
   config.formatter = :documentation
